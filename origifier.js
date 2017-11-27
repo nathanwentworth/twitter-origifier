5f1572db2517e24e9b815f33e6b9caf58470103f
Nathan Wentworth <nwentworth95@gmail.com>
2017-11-27 09:56:48 -0500

    added 'name' support, v1.3

--- origifier.js
+++ origifier.js
@@ -2,7 +2,10 @@
   var u = window.location.href;
   var suff = u.search(/:\w+/ig);
   var base = (suff > -1) ? u.substring(0, suff) : u;
-  base = base.replace('?format=', '.');
+  var format = /[?&]format=/ig;
+  var name = /[?&]name=\w+/ig;
+  base = base.replace(format, '.');
+  base = base.replace(name, '');
   if (!u.includes(':orig')) { base += ':orig'; }
-  if (!u.includes(':orig') || u.includes('?format=')) { window.location.replace(base); }
+  if (!u.includes(':orig') || u.match(format) || u.match(name)) { window.location.replace(base); }
 })();
